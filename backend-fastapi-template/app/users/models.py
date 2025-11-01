10ae0147fdaa4bc4bf2d2346054f78e824a2b0d2
eigenlambda123 <rmvilla987@gmail.com>
2025-11-02 02:59:06 +0800

    added user role boilerplate

--- backend-fastapi-template/app/users/models.py
+++ backend-fastapi-template/app/users/models.py
@@ -1,7 +1,13 @@
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from enum import Enum
+
+class RoleEnum(str, Enum):
+    ADMIN = "admin"
+    USER = "user"
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str
     email: str
+    role: RoleEnum = Field(default=RoleEnum.USER)
